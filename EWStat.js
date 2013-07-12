4ea117ffb26ba310b8bd13105c1292de6e3d27ad
Elliot Winard <github@caveteen.com>
2013-07-11 23:40:39 -0400

    add mean and median, update syntax for adding samples and getting sample count

--- EWStat.js
+++ EWStat.js
@@ -8,19 +8,54 @@ if (typeof module === 'undefined') { var module = {exports: {}}; }
 
 function EWStat() {
     "use strict";
-    var samples = [];
-    
-    // sample is a javascript object w/any number of features
+    var samples = [], // array of samples (objects)
+        fields = {}; // array of arrays of field values
+
+    // helper functions
+    function isEven (num) { return num%2 === 0; }    
+    function isOdd (num) { return !isEven(num); }
+
+    // sample is a javascript object w/any number of fields
     this.addSample = function (sample) {
         samples.push(sample);
+        for (var i in sample) {
+            if (!fields[i]) fields[i]=[];
+            fields[i].push(sample[i]);
+        };
     };
+
+    this.getSample = function (num) {
+        return samples[num];
+    }; // getSample
 
     this.getSamples = function () {
         return samples;
     }; // getSamples
 
+    this.__defineGetter__("count", function() { return samples.length; });
+    
     this.dump = function () {
         console.log(samples);
+    };
+
+    this.mean = function (field) {
+        var sampleArr=fields[field], sum=0;
+        for (var i=0;i<sampleArr.length;i++) {
+            sum+=sampleArr[i];
+        }
+        return sum/samples.length;
+    };
+
+    this.median = function (field) {
+        var count = samples.length,
+            fieldArr = fields[field].sort(function(a,b) {return a-b;});
+        if (isOdd(count)) { // there is a midpoint
+            return fieldArr[(count-1)/2];
+        } else { // even length so average
+            var midpoint0 = fieldArr[(count/2-1)],
+                midpoint1 = fieldArr[count/2];
+            return (midpoint0 + midpoint1)/2;
+        }
     };
 } // EWStat
 
--- spec/EWStat.spec.js
+++ spec/EWStat.spec.js
@@ -4,17 +4,40 @@
 // https://npmjs.org/package/jasmine-node-karma
 "use strict";
 var EWStat = require('../EWStat').EWStat;
-var stat = new EWStat();
 describe('EWStat Suite', function () {
 
     function makeObject(a, b, c) {
-        return {a:a,b:b,c:c};
+        return {a: a, b: b, c: c};
     }
     it('accepts samples',
         function () {
-           stat.addSample(makeObject(1,2,3));
-           var samples=stat.getSamples();
-           expect(samples.length).toBe(1);
+            var stat = new EWStat();
+            stat.addSample(makeObject(1, 2, 3));
+            var samples = stat.getSamples();
+            expect(samples.length).toBe(1);
+            expect(stat.count).toBe(1);
         });
-    }
-);
+    it('measures mean correctly',
+       function () {
+           var stat = new EWStat();
+           for (var i=0;i<9;i++) {
+               stat.addSample(makeObject(1*i, 2*i, 3*i));
+           }
+           expect(stat.count).toBe(9);
+           expect(stat.mean("a")).toBe(4);
+           expect(stat.mean("b")).toBe(8);
+           expect(stat.mean("c")).toBe(12);
+        });
+    it('measures median correctly', // happens to be the same as the mean in this example
+       function () {
+           var stat = new EWStat();
+           for (var i=0;i<9;i++) {
+               stat.addSample(makeObject(1*i, 2*i, 3));
+           }
+           expect(stat.count).toBe(9);
+           expect(stat.median("a")).toBe(4);
+           expect(stat.median("b")).toBe(8);
+           expect(stat.median("c")).toBe(3);
+        });
+     }
+ );
